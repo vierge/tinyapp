2976f8ac29b74a5411974370bdd87c14f8311d56
vierge <marancholia@gmail.com>
2020-01-21 17:24:01 +0000

    added body-parser to project, added urls/new/ and route handlers

--- express_server.js
+++ express_server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
 
+const generateRandomString = () => {
+  Math.floor((Math.random() + 1).toString(36).substring(6));
+}
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("got 'em!");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
