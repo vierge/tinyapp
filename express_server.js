dd4060b27d747a71fb34b36dd3ebf47bad78c926
vierge <marancholia@gmail.com>
2020-01-25 21:58:54 +0000

    eslint pass

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ app.use(cookieSession({
 const urlDatabase = {
   "b2xVn2": { longURL: "http://lighthouselabs.ca", userid: "lonley" },
   "9sm5xk": { longURL: "http://google.com", userid: "lonlon" }
-}
+};
 
 const users = {
   lonley: {
@@ -25,13 +25,14 @@ const users = {
     email: "forever@alone.me",
     hashedPassword: bcrypt.hashSync("iwantahug", 10)
   }
-}
+};
 
 //FUNCTIONS
 
+
 const generateRandomString = () => {
   return (Math.random() + 1).toString(36).substring(6);
-}
+};
 
 const validator = (value, property) => {
   console.log(`CURRENT USERS:`);
@@ -41,28 +42,29 @@ const validator = (value, property) => {
     console.log(`COMPARING KEY ${users[key][property]} to ${value}`);
     if (users[key][property] === value) {
       return users[key];
-    };
+    }
   }
   return false;
-}
+};
+
+
 
 const urlsForUser = (id) => {
   const filteredUrls = {};
-  for (key in urlDatabase) {
-    // console.log(`CHECKING ${urlDatabase[key].userid}`);
-    if (urlDatabase[key].userid === id.user_id) {
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userid === id.userId) {
       filteredUrls[key] = urlDatabase[key];
     }
   }
   return filteredUrls;
-}
+};
 
 //GETS
 
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlsForUser(req.session),
-    loggedIn: req.session.user_id
+    loggedIn: req.session.userId
   };
   console.log(urlsForUser(req.session));
   res.render("urls_index", templateVars);
@@ -70,9 +72,9 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    loggedIn: req.session.user_id
-  }
-  if (req.session.user_id) {
+    loggedIn: req.session.userId
+  };
+  if (req.session.userId) {
     res.render("urls_new", templateVars);
   } else {
     res.redirect("/login");
@@ -83,28 +85,29 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
-    loggedIn: req.session.user_id
+    loggedIn: req.session.userId
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/register", (req, res) => {
   let templateVars = {
-    loggedIn: req.session.user_id
-  }
+    loggedIn: req.session.userId
+  };
   res.render("urls_register", templateVars);
 });
 
 app.get("/login", (req, res) => {
   let templateVars = {
-    loggedIn: req.session.user_id
-  }
+    loggedIn: req.session.userId
+  };
   res.render("urls_login", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  res.redirect(`${urlDatabase[req.params.shortURL]}`);
-})
+  urlDatabase[req.params.shortURL].views++;
+  res.redirect(`${urlDatabase[req.params.shortURL].longURL}`);
+});
 
 
 //POST
@@ -113,19 +116,14 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/new", (req, res) => {
   console.log(req.body);
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = { longURL: req.body.longURL, userid: req.session.user_id };
-  res.redirect(`/urls/${shortURL}`);
-
-});
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
+  urlDatabase[shortURL] = { longURL: req.body.longURL, userid: req.session.userId };
   res.redirect("/urls");
+
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
   if (urlsForUser(req.session)[req.params.shortURL]) {
-    console.log(`updating ${req.params.shortURL} to ${req.body}`)
+    console.log(`updating ${req.params.shortURL} to ${req.body}`);
     urlDatabase[req.params.shortURL] = req.body.newURL;
     res.redirect('/urls');
   } else {
@@ -135,13 +133,18 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   console.log("FIRING LOGIN VALIDATOR");
   if (validator(email, "email")) {
     const key = validator(email, "email");
     if (bcrypt.compareSync(password, key.hashedPassword)) {
-      req.session.user_id = key.id;
+      req.session.userId = key.id;
       console.log("cookie created! user logged in!");
       return res.redirect("/urls");
     }
@@ -167,7 +170,7 @@ app.post("/register", (req, res) => {
       email: email,
       hashedPassword: bcrypt.hashSync(password, 10)
     };
-    req.session.user_id = newID;
+    req.session.userId = newID;
     console.log(users);
     res.redirect("/urls");
   } else {
