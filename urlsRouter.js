74506328386d0327435f645598ff3bed94940846
vierge <marancholia@gmail.com>
2020-01-27 19:44:58 +0000

    fixed bugs and collapsed all current functionality into urls_index!

--- urlsRouter.js
+++ urlsRouter.js
@@ -1,5 +1,5 @@
 const { urlDatabase, userList }  = require('./databases');
-const { urlsForUser } = require('./helpers')
+const { urlsForUser, generateRandomString } = require('./helpers')
 const router = require('express').Router();
 
 router.get("/", (req, res) => {
@@ -33,26 +33,30 @@ router.get("/:shortURL", (req, res) => {
 router.post("/new", (req, res) => {
   console.log(req.body);
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = { longURL: req.body.longURL, userid: req.session.userId };
-  res.redirect("/");
+  urlDatabase[shortURL] = { longURL: req.body.longURL, userId: req.session.userId };
+  console.log(urlDatabase);
+  res.redirect("./");
 
 });
 
 router.post("/:shortURL/edit", (req, res) => {
   if (urlsForUser(req.session, urlDatabase)[req.params.shortURL]) {
-    console.log(`updating ${req.params.shortURL} to ${req.body}`);
-    urlDatabase[req.params.shortURL] = req.body.newURL;
-    res.redirect('/');
+    console.log(`updating ${req.params.shortURL} to`);
+    // console.log(req.body);
+    console.log(urlDatabase[req.params.shortURL]);
+    urlDatabase[req.params.shortURL].longURL = req.body.newURL;
+    console.log(urlDatabase[req.params.shortURL]);
+    res.redirect("../");
   } else {
     res.send("you cannot edit this URL!");
-    res.redirect('/');
+    res.redirect("./");
   }
 
 });
 
 router.post("/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  res.redirect("/");
+  res.redirect("../");
 });
 
 module.exports = router;
